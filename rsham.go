48b968d27e761aca07038f9ddefb51db0a271d00
alecdwm <alec@owls.io>
2016-04-09 11:53:57 +0200

    fixed iptablesdrop mode, again

--- rsham.go
+++ rsham.go
@@ -90,7 +90,7 @@ func sshHandleConnection(mode string, nConn net.Conn, config *ssh.ServerConfig) 
 		ip := nConn.RemoteAddr().String()[:strings.LastIndex(nConn.RemoteAddr().String(), ":")]
 
 		sshLog.Info("adding drop rule to iptables for ip", "ip", ip)
-		cmd := exec.Command("iptables", "-I", "INPUT", "-s", "ip", "-j", "DROP")
+		cmd := exec.Command("iptables", "-I", "INPUT", "-s", ip, "-j", "DROP")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			sshLog.Error("error blocking IP", "ip", ip, "error", err, "command output", out)
